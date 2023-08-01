43716b37a071f8a63d375e71e0ef6494c7fc276f
Ayushi Dubey <89633685+ayushid549@users.noreply.github.com>
2023-08-01 20:25:40 +0530

    Time: 0 ms (100.00%) | Memory: 6.2 MB (16.94%) - LeetSync

--- 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.cpp
+++ 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    string toHex(int n)
+    string toHex(unsigned int num)
     {
         unordered_map<int,string> val { {0,"0"},{1,"1"}, {2,"2"},{3,"3"}, {4,"4"},{5,"5"}, {6,"6"},{7,"7"}, {8,"8"},{9,"9"},  {10,"a"},{11,"b"},{12,"c"},{13,"d"},{14,"e"},{15,"f"}
  };
 
-        if(n==0)
+        if(num==0)
         return "0";
-        
-        unsigned int num = n;
+
         string ans="";
+
         while(num>0)
         {
             int r = num%16;
