233b7e6d6c7483b9063d75c4e98b8c83abbf43f9
Ayushi Dubey <89633685+ayushid549@users.noreply.github.com>
2023-08-07 12:54:48 +0530

    Time: 1028 ms (9.41%) | Memory: 0B (100.00%) - LeetSync

--- 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -1,3 +1,3 @@
 select employee_id from Employees where salary<30000 
-and manager_id not in (select distinct employee_id from Employees)
+and manager_id not in (select employee_id from Employees)
 order by employee_id;
